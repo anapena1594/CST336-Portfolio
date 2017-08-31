e6b05b6f0dde4b4b6ba2f53656f0b3c6dc9d52de
Ana Pena <anpena@csumb.edu>
2017-08-31 21:56:59 +0000

    Lab 1

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+       <meta charset=utf-8"/>
+     <title> Ana Pena's Porfolio </title>
+     
+     <link href="style.css" rel="stylesheet" type="text/css" />
+   
+    </head>
+
+    <body>
+        <header>
+            <h1> Ana Pena </h1>
+        <header/>
+        
+        
+        <nav>
+        <hr width= "50%" />
+        <a href="index.html">Home</a>
+        <a href="about.html">About</a>
+        <a href="contact.html">Contact</a>
+        </nav>
+        
+        <br /> <br />
+        
+        <main>
+        
+        <figure id="me">
+            <img src="img/ana.jpg" alt="Picture of Ana Pena" />
+        </figure>
+        
+        <div id="welcomeText">
+            Hello! <br />
+            <p> Thank you for visiting my professional Porfolio. </p>
+            <p> I'm currently a Student at California State University Monterey Bay, and I have an internship at George Lucas Educational Foundation. </p>
+            
+            <br /> <br />
+            
+            <em> "With ordinary talent and extraordaninary 
+            <strong>perseverance</strong>, all things are attainable" </em> <br />
+            -Thomas F. Buxton
+            
+            </div>
+        </main>
+        
+         <footer>
+            
+             <hr>
+             CST 336. 2017&copy; Pena <br />
+             <strong>Disclaimer:</strong> The information in this webpage is fictitous. <br />
+             Is is used for academic purposes only.
+            
+        </footer>    
+        
+        
+    </body>    
+    
+    
+
+</html>
