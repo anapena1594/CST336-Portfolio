5d3a031cde4899754059bfee7ddf35d6553808d2
Ana Pena <anpena@csumb.edu>
2017-08-31 22:25:15 +0000

    update

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         
         <nav>
         <hr width= "50%" />
-        <a href="index.html">Home</a>
+        <a id = "aaa" href="index.php">Home</a>
         <a href="about.html">About</a>
         <a href="contact.html">Contact</a>
         </nav>
